ab9a29ed63f12888c4e629b332f171e260f60377
Roma <roma.patel@ics-global.in>
2022-02-04 16:01:57 +0530

    Added comment in program file

--- OOPS_Examples/AbstractClassAssignment/Program.cs
+++ OOPS_Examples/AbstractClassAssignment/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AbstractClassAssignment
 {
     class Program
     {
+        // main method
         static void Main(string[] args)
         {
             Rectangle rect = new Rectangle(50);
